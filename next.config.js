5b279027b2c1b09346c3bd92c014b96a6ce11741
NekoLuna喵洛娜 <62191675+MiaoLuoNa@users.noreply.github.com>
2024-06-27 01:41:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
                 ]
             },
           {
@@ -19,7 +19,7 @@ const nextConfig = {
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
                 ]
             }
         ]
